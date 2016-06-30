1e773cf172f56744d9b00d3fc5894bb297792b22
br0ther <afetisov@magecore.com>
2016-06-30 22:12:34 +0300

    TIcket A: first commit - improve

--- ClassA.php
+++ ClassA.php
@@ -10,6 +10,58 @@ class ClassA
 {
     protected $a;
 
+    /**
+     * @return mixed
+     */
+    public function getA()
+    {
+        return $this->a;
+    }
+
+    /**
+     * @param mixed $a
+     */
+    public function setA($a)
+    {
+        $this->a = $a;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getB()
+    {
+        return $this->b;
+    }
+
+    /**
+     * @param mixed $b
+     */
+    public function setB($b)
+    {
+        $this->b = $b;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getC()
+    {
+        return $this->c;
+    }
+
+    /**
+     * @param mixed $c
+     */
+    public function setC($c)
+    {
+        $this->c = $c;
+    }
+
     private $b;
+
+    private $c;
+
+
 
 }
